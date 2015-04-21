7e0321f7b92bb1fa4d9eaa0bfce98ad0d33adc09
Arkadiusz Kwaśny <arkadiusz.kwasny@gmail.com>
2015-04-21 19:49:47 +0200

    commit nasz :)

---
+++ Assets/OnButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnButton : MonoBehaviour {
+
+	public GameObject obstacle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			this.GetComponent<Animator>().Play("button press anim");
+			obstacle.GetComponent<Animator>().Play("obstacle clear");
+		}
+	}
+	void StopAnim()
+	{
+		animation.Stop ();
+
+	}
+}
