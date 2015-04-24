6af142e117e79630d2e074802266f20b6cb2cece
Maareks <Maareks@wp.pl>
2015-04-24 19:22:25 +0200

    granaty

---
+++ Assets/Throw.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour {
+	public GameObject granat;
+	// Use this for initialization
+	void Start () {
+		granat.rigidbody.AddForce ((transform.forward + (Vector3.up * 0.5f))*10 , ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
